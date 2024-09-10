a0e84350bdb014081c10123cb493f87c574b4bfd
JAGATHISH KUMAR <jagathishkumar6@gmail.com>
2024-09-10 11:27:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function showTime(){
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let sec = currentTime.getSeconds();
+    let AM_PM = "AM"
+
+    if (hours >= 12){
+        if (hours > 12) hours-=12;
+        AM_PM = "PM";
+    }
+    else if(hours == 0){
+        hrs = 12;
+        AM_PM = "AM"
+    }
+
+    hours = hours < 10 ? "0" + hours:hours;
+    minutes = minutes < 10 ? "0" + minutes:minutes;
+    sec = sec < 10 ? "0" + sec: sec;
+
+
+    let Time = hours + ":" + minutes + ":" + sec + " " + AM_PM;
+    document.getElementById("clock").innerHTML = Time;
+}
+
+showTime();
+setInterval(showTime,1000);
